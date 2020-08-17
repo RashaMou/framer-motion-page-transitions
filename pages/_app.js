aed0a2e282c64c822954da4d51cc6c91445dc3d0
Ye Joo Park <subwaymatch@gmail.com>
2020-08-17 03:22:39 -0500

    Follow through the official example repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
+
+function handleExitComplete() {}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <>
+      <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
+
+      <style>
+        {`
+        body {
+          padding: 0;
+          margin: 0;
+          background: #f9fbf8;
+        }
+
+        * {
+          box-sizing: border-box;
+          font-family: Helvetica, sans-serif;
+          font-weight: 900;
+          color: #222;
+        }
+      `}
+      </style>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
